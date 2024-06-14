57edd82f8310efb3e4d5f7cef2f42be2a4696060
Chi-naza <ugwuokechinazajames@gmail.com>
2024-06-14 11:39:39 +0100

    Worked on the view and UI of capture screen

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,7 @@ class _CaptureScreenState extends State<CaptureScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        centerTitle: true,
         title: Text(
           "Enigma App Expresso",
           style: TextStyle(
@@ -54,57 +55,120 @@ class _CaptureScreenState extends State<CaptureScreen> {
             fontWeight: FontWeight.bold,
           ),
         ),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _image == null
-                ? const Icon(
-                    Icons.image,
-                    size: 200,
-                  )
-                : Card(
-                    margin: const EdgeInsets.symmetric(horizontal: 30),
-                    child: Column(
-                      children: [
-                        Container(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 43),
+              _image == null
+                  ? Card(
+                      color: Colors.white,
+                      margin: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Column(
+                        children: [
+                          Container(
+                            // margin: EdgeInsets.all(10),
+                            // width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.height * 0.45,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                              image: const DecorationImage(
+                                  image:
+                                      AssetImage("assets/images/noimage.png"),
+                                  fit: BoxFit.cover),
+                            ),
+                            // child: Image.file(_image!),
+                          ),
+                          const SizedBox(height: 13),
+                          const Text(
+                            'Image',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(
+                              "No Image Details To Display",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.red.withOpacity(0.4),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 13),
+                        ],
+                      ),
+                    )
+                  : Card(
+                      color: Colors.white,
+                      margin: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Column(
+                        children: [
+                          Container(
+                            height: MediaQuery.of(context).size.height * 0.45,
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                  image: FileImage(_image!), fit: BoxFit.cover),
                             ),
-                            child: Image.file(_image!)),
-                        const SizedBox(height: 13),
-                        const Text(
-                          'Image',
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w900,
+                            // child: Image.file(_image!),
                           ),
-                        ),
-                        Text(
-                          _image!.path,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontSize: 11,
-                            color: Colors.green,
-                            fontWeight: FontWeight.bold,
+                          const SizedBox(height: 10),
+                          const Text(
+                            'Image',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w900,
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 13),
-                      ],
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(
+                              _image!.path,
+                              textAlign: TextAlign.center,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 13),
+                        ],
+                      ),
                     ),
-                  ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: pickAnImage,
-              child: const Text('Choose An Image From Gallery'),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () => pickAnImage(fromCamera: true),
-              child: const Text('Use The Camera Instead'),
-            ),
-          ],
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: pickAnImage,
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                child: const Text('Choose An Image From Gallery'),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () => pickAnImage(fromCamera: true),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                child: const Text('Use The Camera Instead'),
+              ),
+              const SizedBox(height: 30),
+            ],
+          ),
         ),
       ),
     );
